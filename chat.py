653004da5a5472ac46691f78849853599b95c19e
Kevv-J <kevinjohn1999@gmail.com>
2018-04-29 21:57:19 +0530

    Added Basic chat.py To create a Server

---
+++ chat.py
@@ -0,0 +1,33 @@
+#This Program Creates a Server which accepts messages from client and Sends that message to each client
+
+
+import socket
+import threading
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.bind(('0.0.0.0', 10000))
+
+sock.listen(1)
+
+connections = [] 
+
+def handler(c,a):
+	global connections
+	while True:
+		data = c.recv(1024)
+		for connection in connections:
+			connection.send(bytes(data))
+			
+		if not data:
+			connections.remove(c)
+			c.close()
+			break
+
+while True:
+	c,a = sock.accept()
+	cThread = threading.Thread(target=handler,args=(c,a) )
+	cThread.daemon = True
+	cThread.start()
+	connections.append(c)
+	print(connections)
